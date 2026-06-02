95532216b20f55e24fce3161bc9a42d6743bfb2f
krtrnzg <116386383+krtrnzg@users.noreply.github.com>
2026-06-02 15:47:19 +0300

    Initial Commit

---
+++ StokTakip/StokTakipApp.swift
@@ -0,0 +1,17 @@
+//
+//  StokTakipApp.swift
+//  StokTakip
+//
+//  Created by Vahit Emre Tellier on 2.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct StokTakipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
